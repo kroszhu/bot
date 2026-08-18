ec6cb19501dd21d1eac21b8b6bf3884d124c000f
kros <2450866102@qq.com>
2026-08-19 00:43:07 +0800

    init

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+# cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --parallel
